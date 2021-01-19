afe02356f523443cc7ecb554ac80119bca4fd1be
PapaBear4 <38929220+PapaBear4@users.noreply.github.com>
2021-01-18 22:48:22 -0500

    Update rosinstall.sh
    added line to snap install VS Code

--- rosinstall.sh
+++ rosinstall.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 # this is a setup script to be run at the initiation of an AWS instance that will get it ready to run ROS noetic
-# cp this into the cmd line to run the script
+# copy this into the cmd line to run the script
 # $wget -O - https://raw.githubusercontent.com/PapaBear4/my_aws_ros_setup/main/rosinstall.sh | sudo bash
-
-#first, set your user password so you can log in via RDP once all this is done
-#passwd ubuntu
 
 #add the ROS thing to the apt list and get the key
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -14,4 +11,5 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31
 apt update
 apt dist-upgrade -y
 apt install xrdp cinnamon firefox ros-noetic-desktop-full python3-rosdep -y
+snap install code --classic
 echo "Done installing ROS.  Please set your password, reboot your machine, and log in from RDP."
