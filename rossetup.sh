51b5cd21d63a4845195d5253313ae9334468feac
PapaBear4 <38929220+PapaBear4@users.noreply.github.com>
2021-01-18 23:07:49 -0500

    Update rossetup.sh
    comments for clarity

--- rossetup.sh
+++ rossetup.sh
@@ -2,11 +2,15 @@
 #this script will get ROS setup and ready for use
 # $wget -O - https://raw.githubusercontent.com/PapaBear4/my_aws_ros_setup/main/rossetup.sh | sudo bash
 
+#set up rosdep
 rosdep init
 rosdep update
+
+#set git global values
 git config --global user.name "PapaBear4"
 git config --global user.email "bear.sidell@gmail.com"
 
+#add to bash path
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
