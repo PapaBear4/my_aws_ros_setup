8a2acd00ac45062c3e730e570bc721bf0ade9aea
PapaBear4 <38929220+PapaBear4@users.noreply.github.com>
2021-01-18 22:58:33 -0500

    Update rossetup.sh
    added rosdep lines

--- rossetup.sh
+++ rossetup.sh
@@ -2,6 +2,8 @@
 #this script will get ROS setup and ready for use
 # $wget -O - https://raw.githubusercontent.com/PapaBear4/my_aws_ros_setup/main/rossetup.sh | sudo bash
 
+rosdep init
+rosdep update
 git config --global user.name "PapaBear4"
 git config --global user.email "bear.sidell@gmail.com"
 
